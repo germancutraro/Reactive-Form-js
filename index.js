12e90347219ea6c0e88633174460209690b81356
German Cutraro <germancutraro@hotmail.com>
2018-02-01 14:29:01 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+((c, d) => {
+  const form = d.forms[0],
+    inputs = d.querySelectorAll('[required]')
+  c(inputs)
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    alert('Se ha enviado el formulario')
+  })
+  inputs.forEach(input => {
+    let span = d.createElement('span')
+    span.id = input.name
+    input.insertAdjacentElement('afterend', span)
+    input.addEventListener('keyup', e => {
+      if (input.pattern) {
+        let regex = new RegExp(input.pattern)
+        return (!regex.exec(input.value))
+          ? d.querySelector(`#${input.name}`).textContent = `Invalid format, write ${input.title}`
+          : d.querySelector(`#${input.name}`).textContent = null
+      } else {
+        return (!input.value)
+          ? d.querySelector(`#${input.name}`).textContent = `${input.title} is required`
+          : d.querySelector(`#${input.name}`).textContent = null
+      }
+    })
+  })
+})(console.log, document);
